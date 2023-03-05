a94dbdba04722f52735a668ef1b184b78a16aaf5
EINDEX <snowstarlbk@gmail.com>
2023-03-05 14:50:59 +0800

    refactor: clean up dependencies

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,7 @@
-import { LSPluginUserEvents } from "@logseq/libs/dist/LSPlugin.user";
-import React from "react";
-
-let _visible = logseq.isMainUIVisible;
-
-function subscribeLogseqEvent<T extends LSPluginUserEvents>(
-  eventName: T,
-  handler: (...args: any) => void
-) {
-  logseq.on(eventName, handler);
-  return () => {
-    logseq.off(eventName, handler);
-  };
-}
-
-const subscribeToUIVisible = (onChange: () => void) =>
-  subscribeLogseqEvent("ui:visible:changed", ({ visible }) => {
-    _visible = visible;
-    onChange();
-  });
-
-export const useAppVisible = () => {
-  return React.useSyncExternalStore(subscribeToUIVisible, () => _visible);
-};
-
-
-export const sleep = (waitSec: number) : Promise<void> => {
+export const sleep = (waitSec: number): Promise<void> => {
   return new Promise(function (resolve) {
-
-      setTimeout(function() { resolve() }, waitSec);
-
+    setTimeout(function () {
+      resolve();
+    }, waitSec);
   });
-} 
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import reactPlugin from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import logseqDevPlugin from "vite-plugin-logseq";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [logseqDevPlugin(), reactPlugin()],
+  plugins: [logseqDevPlugin()],
   // Makes HMR available for development
   build: {
     target: "esnext",
