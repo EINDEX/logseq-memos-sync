27c4791be38a6b95ff3acba1c715803f6ca3f29b
Alex Zhao <git@zhaochunqi.com>
2023-07-23 03:50:23 +0800

    add support for files not using external link.

--- src/memos/utils.ts
+++ src/memos/utils.ts
@@ -1,5 +1,5 @@
 import { IBatchBlock } from "@logseq/libs/dist/LSPlugin";
-import { Mode } from "../settings";
+import { Mode, Visibility } from "../settings";
 import { Memo } from "./type";
 import { format } from "date-fns";
 
@@ -18,6 +18,8 @@ export const formatContentWhenPush = (content: string) => {
 
 export const memoContentGenerate = (
   memo: Memo,
+  host: string,
+  openId: string,
   preferredTodo: string,
   withProperties: boolean = false
 ): IBatchBlock[] => {
@@ -34,12 +36,17 @@ export const memoContentGenerate = (
   const result = content.split(BREAK_LINE).filter((item) => !!item.trim());
 
   const children: IBatchBlock[] = [];
-  if (memo.resourceList.length > 0){
-    for(let i = 0; i < memo.resourceList.length; i++) {
-      let resource = memo.resourceList[i];
-      children.push({
-        content: `![${resource.filename}](${resource.externalLink})`,
-      })
+  if (memo.resourceList.length > 0) {
+    for (const resource of memo.resourceList) {
+      let link;
+      if (resource.externalLink) {
+        link = resource.externalLink;
+      }else if(memo.visibility.toLowerCase() == Visibility.Public.toLowerCase()){
+        link = `${host}/o/r/${resource.id}`;
+      }else{
+        link = `${host}/o/r/${resource.id}?openId=${openId}`;
+      }
+      children.push({content: `![${resource.filename}](${link})`});
     }
   }
 
