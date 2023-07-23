7ad03c4d67fe244befef32306fe023854e1fe7b9
EINDEX <snowstarlbk@gmail.com>
2023-07-23 10:49:48 +0800

    test: add testcase and ci for resource sync logic

--- src/memos/type.ts
+++ src/memos/type.ts
@@ -1,16 +1,27 @@
+type Base = {
+  updatedTs: number;
+  createdTs: number;
+};
+
 export type Resource = {
   id: number;
   filename: string;
   externalLink: string;
-}
+  type: string;
+  size: number;
+  linkedMemoAmount: number;
+} & Base;
 
 export type Memo = {
   content: string;
   id: number;
   rowStatus: string;
-  updatedTs: number;
   visibility: string;
   displayTs: number;
-  createdTs: number;
+  creatorId: number;
+  pinned: boolean;
+  creatorName: string;
+  creatorUsername: string;
   resourceList: Resource[];
-};
+  relationList: [];
+} & Base;
